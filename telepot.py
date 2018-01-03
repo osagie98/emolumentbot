89c5d65b3590a5edf88ef2897f6781d2eb07f3e2
Augustine Osagie <osagie@umich.edu>
2018-01-02 21:52:48 -0500

    heroku testing

--- telepot.py
+++ telepot.py
@@ -49,7 +49,17 @@ def main():
     updater.dispatcher.add_error_handler(error)
 
     # Start the Bot
-    updater.start_polling()
+    #updater.start_polling()
+    import os
+
+    TOKEN = "TOKEN422005629:AAGAQNWUHuRYEC8ezlTfgJfPHrvARKAu4sg"
+    PORT = int(os.environ.get('PORT', '8443'))
+    updater = Updater(TOKEN)
+    # add handlers
+    updater.start_webhook(listen="0.0.0.0",
+                          port=PORT,
+                          url_path=TOKEN)
+    updater.bot.set_webhook("https://arcane-everglades-80300.herokuapp.com/" + TOKEN)
 
     # Run the bot until the user presses Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT
